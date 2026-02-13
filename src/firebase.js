4b90a2401be75dd5cceb45ae0f52cc808f6f01ac
RehemaU <95334050+RehemaU@users.noreply.github.com>
2026-02-13 17:45:14 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,14 +8,7 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBVhOanqAHb2nvL6sTM_UoBkCMqDffvdxQ",
-    authDomain: "bit-buddy-f6a64.firebaseapp.com",
-    databaseURL: "https://bit-buddy-f6a64-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "bit-buddy-f6a64",
-    storageBucket: "bit-buddy-f6a64.firebasestorage.app",
-    messagingSenderId: "363918521619",
-    appId: "1:363918521619:web:52e511d1ca708c6fa996e6",
-    measurementId: "G-CZG92P861D"
+
 };
 
 // Initialize Firebase
